114554795f06b962a68e201aa5b476ad59371a00
user <user@example.com>
2025-09-08 14:23:45 +0300

    create basic server

---
+++ server/index.ts
@@ -0,0 +1,16 @@
+import type {Request, Response} from "express";
+
+declare function require(module: string): any;
+const express = require('express');
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({msg: "Hello from server!"})
+})
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});

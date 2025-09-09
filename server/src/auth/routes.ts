320200d996cfa157ef811c9bfb9c3672fca2ba4c
user <user@example.com>
2025-09-09 16:14:32 +0300

    add user login back

--- server/src/auth/routes.ts
+++ server/src/auth/routes.ts
@@ -2,10 +2,14 @@ import type {Request, Response} from "express";
 import type {Users} from "../../generated/prisma";
 
 declare function require(module: string): any;
+
 const {Router} = require("express");
 const crypto = require("crypto");
 const {PrismaClient} = require('../../generated/prisma')
 const {v4: uuid} = require('uuid');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 
 const authRouter = Router();
 const client = new PrismaClient();
@@ -28,4 +32,25 @@ authRouter.post('/register', (req: Request, res: Response) => {
     })
 })
 
-module .exports = authRouter;
+authRouter.post('/login', async (req: Request, res: Response) => {
+    const {username, password} = req.body;
+    client.users.findUnique({where: {username}})
+        .then((row: Users) => {
+            if (!row) return res.status(404).json({error: 'User not found'});
+            crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function (err: Error, hashedPassword: Buffer) {
+                if (err) {
+                    throw new Error(err.message);
+                }
+                if (!crypto.timingSafeEqual(hashedPassword, row.password)) {
+                    return res.status(401).json({error: 'Authentication failed'});
+                }
+                const accessToken = jwt.sign({userId: row.id}, process.env.JWT_SECRET, {expiresIn: '15m'})
+                const refreshToken = jwt.sign({userId: row.id}, process.env.JWT_SECRET)
+                return res.status(200)
+                    .cookie('refreshToken', refreshToken, {httpOnly: true})
+                    .json({accessToken});
+            });
+        }).catch((err: Error) => res.status(500).send({error: err.message}));
+});
+
+module.exports = authRouter;

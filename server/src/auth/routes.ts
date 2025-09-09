68135c8ce21f1eea854988dcc812cc2fa0618a03
user <user@example.com>
2025-09-09 12:23:50 +0300

    add user register back

---
+++ server/src/auth/routes.ts
@@ -0,0 +1,29 @@
+import type {Request, Response, NextFunction} from "express";
+import type {Users} from "../../generated/prisma";
+
+declare function require(module: string): any;
+const {Router} = require("express");
+const crypto = require("crypto");
+const {PrismaClient} = require('../../generated/prisma')
+const {v4: uuid} = require('uuid');
+
+const authRouter = Router();
+const client = new PrismaClient();
+
+authRouter.post('/register', (req: Request, res: Response, next: NextFunction) => {
+    const {username, email, password} = req.body;
+    const salt = crypto.randomBytes(16)
+    crypto.pbkdf2(password, salt, 310000, 32, 'sha256', function (err: Error, hashedPassword: Buffer) {
+        if (err) throw new Error(err.message);
+        client.users.create({
+            data: {
+                id: uuid(),
+                username,
+                password: hashedPassword,
+                salt,
+                email
+            }
+        }).then((user: Users) => res.status(201).send({user}))
+            .catch((err: Error) => res.status(500).send({error: err.message}))
+    })
+})

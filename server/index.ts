476b4e729d999b5cb3b8d25e15c88a721205d504
user <user@example.com>
2025-10-01 14:15:50 +0300

    webhook setup attempt #5

--- server/index.ts
+++ server/index.ts
@@ -8,8 +8,6 @@ if (!process.env.LOCAL) app.use('/telegram-webhook', webhookCallback(bot));
 console.log(process.env.FRONTED_URL)
 
 app.listen(5000, async () => {
+    process.env.LOCAL && await bot.start();
     console.log(`Server started on port 5000`);
-    process.env.LOCAL
-        ? bot.start()
-        : await bot.api.setWebhook(`${process.env.FRONTED_URL}/telegram-webhook`);
 });

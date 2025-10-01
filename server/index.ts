cd66cb6138105ae2f989cca5f101ba5be7574d3e
user <user@example.com>
2025-10-01 14:02:40 +0300

    webhook setup attempt #4

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ const bot = require('./src/config/telegramBot')
 require('dotenv').config();
 
 if (!process.env.LOCAL) app.use('/telegram-webhook', webhookCallback(bot));
+console.log(process.env.FRONTED_URL)
 
 app.listen(5000, async () => {
     console.log(`Server started on port 5000`);
